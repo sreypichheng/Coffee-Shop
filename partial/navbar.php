a56a076865db4e43ec40cd88b9e9711c7f07f2b5
SreyPich <sreypich.heng@student.passerellesnumeriques.org>
2021-08-18 14:25:20 +0700

    indexadd

--- partial/navbar.php
+++ partial/navbar.php
@@ -1,3 +1,5 @@
+
+
 <nav class="navbar navbar-expand-lg navbar-warning  bg-warning">
 <a class="navbar-brand" href="#">
     <img src="https://freepikpsd.com/media/2019/10/coffee-logo-png-7-Transparent-Images.png" width="40" height="40" class="d-inline-block align-top" alt="">
@@ -8,13 +10,13 @@
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="?page=home">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="?page=home">HOME <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="?page=coffee_html">Coffee</a>
+        <a class="nav-link" href="?page=coffee_html">COFFEE</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="?page=about_html">About</a>
+        <a class="nav-link" href="?page=about_html">ABOUT</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -23,9 +25,5 @@
     </form>
   </div>
 </nav>
-<div class="card mb-3">
-  <img src="https://assets.visme.co/templates/banners/thumbnails/i_Coffee-Twitter-Header_full.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-   
-  </div>
-</div>
+
+
